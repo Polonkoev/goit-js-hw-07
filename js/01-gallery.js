34280c7c68776c166a3b3f948107f26ac84e5079
Polonkoev <103013345+Polonkoev@users.noreply.github.com>
2022-09-18 23:17:46 +0600

    1 task completed

---
+++ js/01-gallery.js
@@ -0,0 +1,58 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const divGallery = document.querySelector(".gallery");
+
+function createImage(item) {
+  const { preview, original, description } = item;
+  divGallery.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source= "${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+  );
+}
+
+galleryItems.map(createImage);
+
+
+function clickHandle(event) {
+  event.preventDefault();
+
+  const imgLink = event.target.dataset.source;
+
+  if (imgLink !== undefined) {
+    const originalImgHTML = `
+    <img src="${imgLink}" width="100%" height="100%">
+  `
+    const instance = basicLightbox.create(originalImgHTML
+      ,
+      {
+        onShow: () => {
+          window.addEventListener("keydown", closeByEsc);
+        },
+        onClose: () => {
+          window.removeEventListener("keydown", closeByEsc);
+        },
+      }
+    );
+
+    instance.show();
+
+    function closeByEsc(event) {
+      if (event.code === "Escape") {
+        instance.close();
+      }
+      window.removeEventListener("keydown", closeByEsc);
+    }
+  }
+}
+
+divGallery.addEventListener("click", clickHandle);
